1d25e5fc67713bbf60087f49282225767bd0ae22
mAckmm <makhija.mohit29@gmail.com>
2021-04-21 09:14:46 +0530

    Initial notes

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,26 @@ public class Main {
         System.out.println("Hello Java!");
     }
 }
+
+/* NOTES */
+
+// JDK - Java Development Kit
+// JRE - Java Runtime Environment
+// JVM - Java Virtual Machine
+
+// JDK = JRE + Compiler
+// JRE = JVM + Libraries
+
+/*
+JVM ->
+Loads, verifies and Executes code.
+It does not exist physically and is platform independent.
+
+JRE ->
+JVM + libraries that JVM uses at runtime.
+It physically exists and is platform dependent.
+
+JDK ->
+Full featured software development kit.
+Contains JRE and development tools.
+*/
